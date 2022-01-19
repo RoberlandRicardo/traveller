3c91edf48098d84fceb907c0ddcc7cc8d19d9641
Daniele Carvalho <danielemcarvalho2@gmail.com>
2022-01-19 11:13:03 -0300

    estados da home

--- lib/app/views/home.dart
+++ lib/app/views/home.dart
@@ -10,9 +10,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String _location = 'Rio Grande do Norte, Brasil';
+  String _state = 'not_travel';
+  String location = 'Rio Grande do Norte, Brasil';
+  String name = 'Fulano';
+  String _coin = '5,23';
+  String _hour = '17:24';
+  String _date = '02/01/2022';
+  String _temperature = '12 C';
+  String _time = 'Parcialmente nublado';
+  String _countDays = '2 meses, 7 dias e 10 horas';
+  String tripName = 'Rio de Janeiro';
 
-  clickHere() {}
+  @protected
+  @mustCallSuper
+  void initState() {
+    calcDate();
+  }
+
+  infoTravel() {}
+
+  calcDate() {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +50,7 @@ class _HomeState extends State<Home> {
                 child: Row(children: [
                   Icon(Icons.location_on,
                       color: Color.fromRGBO(244, 54, 27, 1)),
-                  Text(_location)
+                  Text(location)
                 ]),
               ),
               Spacer(),
@@ -57,81 +76,44 @@ class _HomeState extends State<Home> {
                             fontWeight: FontWeight.bold, fontSize: 34),
                       ),
                       Text(
-                        'Fulano',
+                        name,
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 34),
                       ),
                     ]),
               ),
               Spacer(),
-              Container(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text('Dolár americano'),
-                      Text(
-                        '5,53',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 24),
-                      ),
-                      Text('reais')
-                    ]),
-              ),
-            ],
-          ),
-          Spacer(),
-          Container(
-            child: Column(children: [
-              Text(
-                'Veja informações sobre sua viagem',
-              ),
-              Text(
-                "clique aqui",
-                style: TextStyle(
-                  color: Color.fromRGBO(244, 54, 27, 1),
-                  fontWeight: FontWeight.bold,
+              if (_state == 'travel')
+                Container(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text('Dolár americano'),
+                        Text(
+                          _coin,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 24),
+                        ),
+                        Text('reais')
+                      ]),
                 ),
-              )
-            ]),
-          ),
-          Spacer(),
-          SvgPicture.asset(
-            'assets/svgs/cuate.svg',
-            width: 270.0,
-            height: 270.0,
-          ),
-          Spacer(),
-          Row(
-            children: [
-              Container(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Horário do local'),
-                      Text(
-                        '17:24',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 24),
-                      ),
-                      Text('02/01/2022'),
-                    ]),
-              ),
-              Spacer(),
-              Container(
-                child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text('Hoje'),
-                      Text(
-                        '12 C',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 24),
-                      ),
-                      Text('Parcialmente nublado')
-                    ]),
-              ),
             ],
           ),
+          Spacer(),
+          getWidgetTextTop(),
+          Spacer(),
+          if (_state == 'not_travel')
+            SvgPicture.asset(
+              'assets/svgs/journey.svg',
+              width: 320.0,
+              height: 320.0,
+            )
+          else
+            SvgPicture.asset('assets/svgs/cuate.svg',
+                width: 280.0, height: 280.0),
+          Spacer(),
+          getWidgetTextBottom(),
+          Spacer()
         ],
       ),
     ));
@@ -139,4 +121,107 @@ class _HomeState extends State<Home> {
 
   @override
   bool get wantKeepAlive => throw UnimplementedError();
+
+  getWidgetTextTop() {
+    if (_state == 'travel') {
+      return Container(
+        child: Column(children: [
+          Text(
+            'Veja informações sobre sua viagem',
+          ),
+          Text(
+            "clique aqui",
+            style: TextStyle(
+              color: Color.fromRGBO(244, 54, 27, 1),
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ]),
+      );
+    } else if (_state == 'before_travel') {
+      return Container(
+        child: Column(children: [
+          Text(
+            'Faltam',
+          ),
+          Text(
+            _countDays,
+            style: TextStyle(
+                color: Color.fromRGBO(244, 54, 27, 1),
+                fontWeight: FontWeight.bold,
+                fontSize: 24),
+          )
+        ]),
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  getWidgetTextBottom() {
+    if (_state == 'travel') {
+      return Row(
+        children: [
+          Container(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text('Horário do local'),
+              Text(
+                _hour,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(_date),
+            ]),
+          ),
+          Spacer(),
+          Container(
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+              Text('Hoje'),
+              Text(
+                _temperature,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(_time)
+            ]),
+          ),
+        ],
+      );
+    } else if (_state == 'before_travel') {
+      return Container(
+        child: Column(children: [
+          Text(
+            'Para',
+          ),
+          Text(
+            tripName,
+            style: TextStyle(
+                color: Color.fromRGBO(244, 54, 27, 1),
+                fontWeight: FontWeight.bold,
+                fontSize: 24),
+          ),
+        ]),
+      );
+    } else {
+      return Container(
+        child: Column(children: [
+          Text(
+            'Você não possui nenhuma viagem ativa no momento.',
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Clique aqui",
+                style: TextStyle(
+                    color: Color.fromRGBO(244, 54, 27, 1),
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(" e cadastre uma nova.")
+            ],
+          )
+        ]),
+      );
+    }
+  }
 }
