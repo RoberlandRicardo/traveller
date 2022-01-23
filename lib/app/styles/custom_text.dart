d648f31c35691120c546d2f587b36e34d5391a92
Daniele Carvalho <danielemcarvalho2@gmail.com>
2022-01-23 12:53:31 -0300

    Tela de perfil - não autenticado

--- lib/app/styles/custom_text.dart
+++ lib/app/styles/custom_text.dart
@@ -5,6 +5,7 @@ class CustomText extends TextTheme {
   static final Color fontColor = Color.fromRGBO(19, 19, 19, 1);
   static final Color fontColorButton = Color.fromRGBO(227, 227, 227, 1);
   static final Color fontColorTopBar = Color.fromRGBO(227, 227, 227, 1);
+  static final Color fontColorOrange = Color.fromRGBO(244, 54, 27, 1);
   static final double fontSizeh1 = 34;
   static final double fontSizeh2 = 24;
   static final double fontSizeSubtitle = 16;
@@ -30,6 +31,13 @@ class CustomText extends TextTheme {
     fontFamily: fontFamily,
     fontSize: fontSizeTopBar,
     color: fontColorTopBar,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle buttonOrange = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: fontSizeButton,
+    color: fontColorOrange,
     fontWeight: FontWeight.w700,
   );
 
--- lib/app/views/perfil.dart
+++ lib/app/views/perfil.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:traveller/app/components/generic_screen_nivel02.dart';
 import 'package:traveller/app/styles/custom_text.dart';
 
 class Perfil extends StatelessWidget {
+  String state = 'not_auth';
   static const List<String> _buttonNames = <String>[
     'Ajuda',
     'Alterar meus dados',
@@ -64,59 +66,114 @@ class Perfil extends StatelessWidget {
     return GenericScreen(
       currendIndex: 3,
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            GestureDetector(
-              child: Container(
-                width: 90,
-                height: 90,
-                decoration: BoxDecoration(
-                  color: CustomText.fontColor,
-                  shape: BoxShape.circle,
-                ),
-                child: Icon(
-                  Icons.person,
-                  size: 80,
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text("Fulano Smith", style: Theme.of(context).textTheme.headline1),
-            Text("Rio Grande do Norte, Brasil",
-                style: Theme.of(context).textTheme.subtitle1),
-            SizedBox(
-              height: 20,
-            ),
-            FractionallySizedBox(
-                widthFactor: 0.9,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                    color: Color.fromRGBO(233, 230, 230, 1),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      ..._buttonNames
-                          .asMap()
-                          .map((index, name) =>
-                              MapEntry(index, ButtonConf(name, index, context)))
-                          .values
-                          .toList()
-                    ],
-                  ),
-                )),
-            SizedBox(
-              height: 20,
-            )
-          ],
+          child: state == 'auth' ? auth(context) : notAuth(context)),
+    );
+  }
+
+  Widget auth(context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
         ),
-      ),
+        GestureDetector(
+          child: Container(
+            width: 90,
+            height: 90,
+            decoration: BoxDecoration(
+              color: CustomText.fontColor,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              Icons.person,
+              size: 80,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text("Fulano Smith", style: Theme.of(context).textTheme.headline1),
+        Text("Rio Grande do Norte, Brasil",
+            style: Theme.of(context).textTheme.subtitle1),
+        SizedBox(
+          height: 20,
+        ),
+        FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                color: Color.fromRGBO(233, 230, 230, 1),
+              ),
+              child: Column(
+                children: <Widget>[
+                  ..._buttonNames
+                      .asMap()
+                      .map((index, name) =>
+                          MapEntry(index, ButtonConf(name, index, context)))
+                      .values
+                      .toList()
+                ],
+              ),
+            )),
+        SizedBox(
+          height: 20,
+        )
+      ],
+    );
+  }
+
+  Widget notAuth(context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 50,
+        ),
+        Container(
+          child: Column(
+            children: [
+              Text('Você não fez o login =(',
+                  style: Theme.of(context).textTheme.headline1),
+              Text('Você não fez a autenticação',
+                  style: Theme.of(context).textTheme.bodyText1)
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        SvgPicture.asset(
+          'assets/svgs/not_auth.svg',
+          width: 320.0,
+          height: 320.0,
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        Container(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Clique aqui ',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontWeight: FontWeight.bold)),
+                  Text('e crie uma conta para salvar seus dados,',
+                      style: Theme.of(context).textTheme.bodyText1)
+                ],
+              ),
+              Text('personalizar seu perfil e visualizar o feed.',
+                  style: Theme.of(context).textTheme.bodyText1)
+            ],
+          ),
+        ),
+        // Spacer(),
+      ],
     );
   }
 }
